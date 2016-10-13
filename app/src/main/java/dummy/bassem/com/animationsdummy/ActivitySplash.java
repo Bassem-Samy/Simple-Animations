d18f4c857386c6b7920a77cee86204fe08e9a68d
OTVENTURES\Bassem.Wissa <www.bassem.wissa@linkdev.com>
2016-10-13 17:28:02 +0200

    kinda arc Signed-off-by: OTVENTURES\Bassem.Wissa <www.bassem.wissa@linkdev.com>

--- app/src/main/java/dummy/bassem/com/animationsdummy/ActivitySplash.java
+++ app/src/main/java/dummy/bassem/com/animationsdummy/ActivitySplash.java
@@ -1,42 +1,69 @@
 package dummy.bassem.com.animationsdummy;
 
 import android.animation.Animator;
-import android.support.v4.widget.TextViewCompat;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.ArcShape;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.ogaclejapan.arclayout.ArcLayout;
 
 import dummy.bassem.com.animationsdummy.helpers.AnimationsHelper;
 
 public class ActivitySplash extends AppCompatActivity {
-    TextView helloTextView;
-    RelativeLayout helloContainer;
-// test git
+    ProgressBar progressBar;
+    FrameLayout progressContainer;
+    final long progressDuration = 250;
+    View arcView;
+    ArcLayout arcLayout;
+
+    // test git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        helloTextView = (TextView) findViewById(R.id.tv_hello);
-        helloContainer = (RelativeLayout) findViewById(R.id.rltv_hello_container);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+        progressContainer = (FrameLayout) findViewById(R.id.frm_progress_container);
+        // arcView = (View) findViewById(R.id.arc_view);
+        arcLayout = (ArcLayout) findViewById(R.id.arc_layout);
+        progressContainer.getViewTreeObserver().addOnGlobalLayoutListener(onGlobalLayoutListener);
         animateHelloToTop();
+       // initializeArc();
 
     }
 
+    private void initializeArc() {
+        long width = progressContainer.getWidth();
+        arcLayout.setRadius((int) (1 * width));
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+       // params.setMargins(0, (int) (-1 *width)+150, 0, 0);
+        arcLayout.setLayoutParams(params);
+    }
+
     private void animateHelloToTop() {
-        AnimationsHelper.moveUpToParent(helloTextView, helloContainer, 1000, true, endOfMoveUpListener);
+        AnimationsHelper.moveUpToParent(progressBar, progressContainer, progressDuration, true, endOfMoveUpListener);
     }
 
     AnimationsHelper.MyAnimationsListener endOfMoveUpListener = new AnimationsHelper.MyAnimationsListener() {
         @Override
         public void onAnimationEnd(Animator animaton) {
             Toast.makeText(ActivitySplash.this, "yo yo yo", Toast.LENGTH_SHORT).show();
+        }
+    };
+    ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+        @Override
+        public void onGlobalLayout() {
+            progressContainer.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+initializeArc();
         }
     };
 
--- app/src/main/java/dummy/bassem/com/animationsdummy/helpers/AnimationsHelper.java
+++ app/src/main/java/dummy/bassem/com/animationsdummy/helpers/AnimationsHelper.java
@@ -18,26 +18,8 @@ public class AnimationsHelper {
     // move vertically by providing dimensions
     public static void moveVertically(final View view, float basePoint, float toPoint, long duration, final MyAnimationsListener endListener) {
         ValueAnimator animator = new ValueAnimator().ofFloat(basePoint, toPoint);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float value = (float) animation.getAnimatedValue();
-                view.setTranslationY(value);
+        completeVerticalAnimaion(animator, view, duration, endListener);
 
-            }
-        });
-        animator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                if (endListener != null)
-                    endListener.onAnimationEnd(animation);
-
-            }
-        });
-
-        animator.setInterpolator(linearInterpolator);
-        animator.setDuration(duration);
-        animator.start();
     }
 
     // move vertically to the top of parent by providing parent only no dimensions and save calculations on the UI side
@@ -69,6 +51,7 @@ public class AnimationsHelper {
         completeVerticalAnimaion(animator, childView, duration, endListener);
     }
 
+    // apply translate y animation
     private static void completeVerticalAnimaion(ValueAnimator animator, final View toAnimateView, final long duration, final MyAnimationsListener endListener) {
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
